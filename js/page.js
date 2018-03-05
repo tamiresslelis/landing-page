314680ba91d7eb01157948782fc34966bf0e7a45
tamiresslelis <tamiresslelis@gmail.com>
2018-03-05 10:22:39 -0300

    page

---
+++ js/page.js
@@ -0,0 +1,15 @@
+
+//change color navbar
+$(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('.navbar').css("background-color", "#004e66")
+            } else {
+                $('.navbar').css("background-color", "transparent" )
+            }
+});
+
+$(function() {
+  $('.carousel').carousel(function(){
+       interval: 300
+   });
+ });
